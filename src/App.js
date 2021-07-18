a22b5482ec4f865bd4a0c5ac885bf6a76cf46e66
Shantanu1099 <sshantanuthakur1099@gmail.com>
2021-07-19 00:41:54 +0530

    Added nav-bar logo input-bar and button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navigation from './Components/Navigation/Navigation'
-import Logo from './Components/Logo/Logo'
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
+      <ImageLinkForm />
     </div>
   );
 }
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import './Logo.css';
+import Brain from './brain.png';
 
 const Logo =  () => {
     return (
         <div className = "ma4 mt0">
-            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-            <div className="Tilt-inner"> 👽 </div>
+            <Tilt className="Tilt br3 shadow-3 " options={{ max : 75 }} style={{ height: 120, width: 150 }} >
+                <div className="Tilt-inner pa3"> <img alt= "galaxy pic" src={Brain} style={{width:'4rem'}}/> </div>
             </Tilt>
         </div>
     );
