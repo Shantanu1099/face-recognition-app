dfd0ed1d4f36ede0680dca857d69671709cb58ed
Shantanu1099 <sshantanuthakur1099@gmail.com>
2021-07-19 04:48:20 +0530

    Updated tilt and Interestnig times

--- src/App.js
+++ src/App.js
@@ -1,13 +1,105 @@
-import Navigation from './Components/Navigation/Navigation'
+import React from 'react';
+import Particles from "react-tsparticles";
+// import Particles from 'react-particles-js';
+import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import Rank from './Components/Rank/Rank';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const ParticleOverlay = {
+    background: {
+      color: {
+        value: "#background: LINEAR-GRADIENT(TO RIGHT, RGB(189, 77, 209), RGB(142, 225, 225));",
+      },
+    },
+    fpsLimit: 60,
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        random: true,
+        value: 5,
+      },
+    },
+    detectRetina: true,
+  
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles
+        className="particle-character"
+        id="tsparticles"
+        // init={this.particlesInit}
+        // loaded={this.particlesLoaded}
+        options={ParticleOverlay}
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -7,7 +7,7 @@ const ImageLinkForm  = () => {
             <p className="f3">{'This Magic Brain Face-Recognition will blow your mind. Give it a try. '}</p>
             <div className="center">
                 <div className="form pa4 br3 shadow-5">
-                    <input className="f4 pa2 w-70 center" type="text" />
+                    <input className="f4 pa2 w-70" type="text" />
                     <button className="w-30 grow f4 br2 link ph3 pv2 dib white bg-light-purple">Detect</button>
                 </div>
             </div>
